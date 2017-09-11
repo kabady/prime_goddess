46486ae8dd82d3575027a748275a54ec3b44321e
李杨 <liyang@liyangdeMacBook-Pro.local>
2017-09-11 18:21:52 +0800

    refs

--- front-src-ts/style/page/rank/index.ts
+++ front-src-ts/style/page/rank/index.ts
@@ -5,6 +5,25 @@ let $ = require( '../../../other-component/build/DomAPI-0.0.4.js' );
 import Page from '../../../other-component/build/Page-0.0.2.js';
 import { APP } from '../../../entry/main.ts';
 
+let _window: any = window;
+console.log(_window.rank_list)
+
+let rank_item_html_template = `
+	<div class="rank-item rank-$idx$">
+		<div class="picture">
+			<img src="$pic$" alt="">
+		</div>
+		<div class="info">
+			<div class="score">
+				<span class="g">$score-int$</span>
+				<span class="g">.</span>
+				<span>$score-point$</span>
+				<span>分</span>
+			</div>
+			<div class="name">$name$</div>
+		</div>
+	</div>
+`;
 export default class HomePage extends Page {
 	app: APP;
 	constructor(app: APP) {
@@ -20,9 +39,24 @@ export default class HomePage extends Page {
 		super(option);
 
 		this.app = app;
+
+		var rank_item_html = '';
+		for (let i = 0, len = _window.rank_list.length; i < len; i++) {
+			let item = _window.rank_list[i];
+			let theItemHtml = rank_item_html_template;
+
+			item['score-int'] = Math.floor(item.score);
+			item['score-point'] = Math.floor(item.score * 10 % 10);
+			for(let key in item){
+				if (item.hasOwnProperty(key)) {
+					theItemHtml = theItemHtml.replace(new RegExp('\\$' + key + '\\$', 'g'), item[key]);
+				}
+			}
+			rank_item_html += theItemHtml;
+		}
+		this.domElem.find('.rank-list').getEl(0).innerHTML = rank_item_html;
 	}
 	event(){
-		
 		this.domElem.find('.longzhu-ad .close-ad').on('click', (e) => {
 			$.pdsp(e);
 
